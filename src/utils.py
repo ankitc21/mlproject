cb90296a5e33a9a2b27309f6ed6f9a6f5dc2c4a4
ankitc21 <ankit004c@gmail.com>
2025-07-13 22:23:50 +0530

    Html file is created

--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,26 @@
-import sys
 import os
-import pickle
-import dill
-import numpy as np
+import sys
+
+import numpy as np 
 import pandas as pd
-from src.exception import CustomException
+import dill
+import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
-def save_object(file_path,obj):
+from src.exception import CustomException
+
+def save_object(file_path, obj):
     try:
-        dir_path=os.path.dirname(file_path)
-        os.makedirs(dir_path,exist_ok=True)
-        with open(file_path,"wb") as file_obj:
-            dill.dump(obj,file_obj)
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
     
 def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
@@ -44,5 +49,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
